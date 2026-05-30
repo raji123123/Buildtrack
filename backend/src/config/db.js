99935e2b541cdb3bf844497d733553a38b216e3d
raji123123 <suhasinimorampudi7@gmail.com>
2026-05-30 14:00:46 +0530

    fix ipv6 connection issue

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'db.ddtuyvbiarewobajwlfu.supabase.co',
+  host: 'aws-1-ap-northeast-2.pooler.supabase.com',
   port: 5432,
   database: 'postgres',
-  user: 'postgres',
+  user: 'postgres.ddtuyvbiarewobajwlfu',
   password: 'Buildtrack@2025',
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  family: 4
 });
 
 pool.connect()
